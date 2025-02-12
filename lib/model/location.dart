0a95a258bdc98b1f94a0b735482a9e1c892a3670
YassenAlmaqtary <67117546+YassenAlmaqtary@users.noreply.github.com>
2025-02-12 10:39:58 +0300

    ss

---
+++ lib/model/location.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/foundation.dart';
+import 'dart:convert';
+
+@immutable
+class Location {
+  final int id;
+  final int customerId;
+  final String latitude;
+  final String longitude;
+  final String? location;
+  final String? additionalDetails;
+  final bool isAvailable;
+  final String createdAt;
+  final String updatedAt;
+
+  const Location({
+    required this.id,
+    required this.customerId,
+    required this.latitude,
+    required this.longitude,
+    this.location,
+    this.additionalDetails,
+    required this.isAvailable,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  factory Location.fromJson(Map<String, dynamic> json) {
+    return Location(
+      id: json['id'],
+      customerId: json['customer_id'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
+      location: json['location'],
+      additionalDetails: json['additional_details'],
+      isAvailable: json['is_available'] == 1,
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'latitude': latitude,
+      'longitude': longitude,
+      'additional_details': additionalDetails,
+    };
+  }
+}
